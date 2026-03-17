64c3ad3a977358c7deda04413a28818c9162a6a9
Kristen Nguyen <114266186+ThiNguyen3008@users.noreply.github.com>
2026-03-17 10:42:37 -0400

    Update api2.js

--- src/services/api2.js
+++ src/services/api2.js
@@ -3,7 +3,7 @@ const BASE = "https://hacker-news.firebaseio.com/v0";
 export async function getNewStoryIds() {
   const res = await fetch(`${BASE}/jobstories.json`);
   const data = await res.json();
-  return data.slice(25, 100);
+  return data.slice(0, 50);
 }
 
 export async function getStory(id) {
